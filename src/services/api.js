adfbc20d5e10792e6de0f5535a3718e7a1b69645
kattabharath12 <106069483+kattabharath12@users.noreply.github.com>
2025-07-09 10:03:35 -0500

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 // src/services/api.js
 import axios from 'axios';
 
-const API_BASE_URL = 'https://tax-bo-production.up.railway.app/api';
+const API_BASE_URL = 'https://tax-bo-production.up.railway.app';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
